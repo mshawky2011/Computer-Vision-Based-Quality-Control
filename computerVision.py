5551237f08ce87f6963a3aa8c3ec180bff3020a0
parthvadhadiya <parthvadhadiya424@gmail.com>
2017-10-05 22:14:29 +0000

    Update computerVision.py

--- computerVision.py
+++ computerVision.py
@@ -18,8 +18,26 @@ params = cv2.SimpleBlobDetector_Params()
 
 params.blobColor= 255
 params.filterByColor = True
-
-
+'''
+params.minThreshold = 10;
+params.maxThreshold = 200;
+ 
+# Filter by Area.
+params.filterByArea = True
+params.minArea = 1500
+ 
+# Filter by Circularity
+params.filterByCircularity = True
+params.minCircularity = 0.1
+ 
+# Filter by Convexity
+params.filterByConvexity = True
+params.minConvexity = 0.87
+ 
+# Filter by Inertia
+params.filterByInertia = True
+params.minInertiaRatio = 0.01
+'''
 # Create a detector with the parameters
 ver = (cv2.__version__).split('.')
 if int(ver[0]) < 3 :
